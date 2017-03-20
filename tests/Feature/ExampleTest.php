f04605a1bd75ff768df0886c3ae5a1feab494c1b
Anthony Van de Gejuchte <anthonyvdgent@gmail.com>
2017-03-20 22:54:27 +0100

    Add a few basic tests

--- tests/Feature/ExampleTest.php
+++ tests/Feature/ExampleTest.php
@@ -14,10 +14,36 @@ class ExampleTest extends TestCase
      *
      * @return void
      */
-    public function testBasicTest()
+    public function testIndexPage()
     {
         $response = $this->get('/');
 
+        $response->assertSee("Laravel");
         $response->assertStatus(200);
     }
+
+    /**
+     * A guest visiting the home screen should be redirected to the login screen
+     *
+     * @return void
+     */
+    public function testHomePage()
+    {
+        $response = $this->get('/home');
+
+        $response->assertStatus(302);
+        $response->assertRedirect('/login');
+    }
+
+    /**
+     * Attempt to see the login screen
+     *
+     * @return void
+     */
+     public function testLoginPage()
+     {
+         $response = $this->get('/login');
+         $response->assertSee('E-Mail Address');
+         $response->assertSee('Password');
+     }
 }
